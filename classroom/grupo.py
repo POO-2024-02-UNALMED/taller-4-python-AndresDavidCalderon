96dcbed9fe3ef9309f6359dba3f432c31c2dfbfa
Andres David Calderon <david.voidgames@gmail.com>
2024-11-24 17:55:54 -0500

    arreglar mutable por defecto

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -12,9 +12,11 @@ class Grupo:
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
+        if lista==None:
+            lista=[]
         lista.append(alumno)
-        self.listadoAlumnos.extend(lista)
+        self.listadoAlumnos = self.listadoAlumnos + lista
 
     def __str__(self):
         return f"Grupo de estudiantes: {self._grupo}"
