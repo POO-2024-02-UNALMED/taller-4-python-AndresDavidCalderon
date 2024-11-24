4e0093bcd1eda05054dc4ad3655862c076b723d4
Andres David Calderon <david.voidgames@gmail.com>
2024-11-24 17:46:02 -0500

    subir respuesta

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,8 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    def __init__(self, nombre, salon="remoto"): # En main se utiliza una vez sin salon.
         self._nombre = nombre
         self._salon = salon
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return f"{self._nombre} {self._salon}"
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,14 +1,14 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = None
+    grado = "Grado 12" # Se espera este formato en el print.
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes=[]): # Estudiantes debe tener valor por defecto
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
@@ -16,11 +16,11 @@ class Grupo:
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return f"Grupo de estudiantes: {self._grupo}"
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
+    def asignarNombre(cls, nombre="Grado 1"): # Se espera que este por defecto sea 1
         cls.grado = nombre
 
     @ classmethod
@@ -28,5 +28,5 @@ class Grupo:
         cls.grado = nombre
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
+    def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
